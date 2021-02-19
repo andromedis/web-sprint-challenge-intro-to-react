cae4eca4837c7b4e2960ab2c94a8a4b2c58cca1b
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-02-19 16:16:06 +0000

    axios get works now

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Character from './components/Character';
 import axios from 'axios';
+import { BASE_URL, PEOPLE } from './constants/constants';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +12,7 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [characters, setCharacters] = useState([]);
-  const [displayId, setDisplayId] = useState(null);
+  const [displayId, setDisplayId] = useState([]);
 
   const openDetails = (id) => {
     setDisplayId(id);
@@ -22,18 +23,39 @@ const App = () => {
   }
 
   useEffect(() => {
-    axios
-      .get(`https://swapi.dev/api/people/`)
-      .then()
-      .catch()
-  })
+    // const addIds = (data) => {
+    //   return data.map(obj => {
+    //     const id = Number(obj.url.substring(28, obj.url.length - 1)) // TODO: 28 is janky, maybe take length of constants added together?
+    //     console.log(id)
+    //     console.log(obj)
+    //     debugger;
+    //     const newObj = {...obj, id: id};
+    //     console.log(id)
+    //     console.log(obj)
+    //     debugger;
+    //     return newObj;
+    //   });
+    // }
+    
+    axios.get(`https://swapi.dev/api/people/`)
+      .then(res =>
+        setCharacters(res.data)
+      )
+      .catch(err =>
+        console.log(err)
+      )
+    // setCharacters(addIds(characters));
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
       {
+        console.log(characters[0])
+      }
+      {
         characters.map(char => {
-          return <Character />
+          return <Character key={char.url} char={char}/>
         })
       }
     </div>
--- src/components/Character.js
+++ src/components/Character.js
@@ -2,8 +2,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Character(props) {
-
+function Character({ char }) {
+    
+    return (
+        <p>
+            {char.name};
+        </p>
+    )
 }
 
 export default Character;
